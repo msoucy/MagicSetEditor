59a925ea7054f55fa76732ad1a1160b9352be903
twanvl <twanvl@0fc631ac-6414-0410-93d0-97cfa31319b6>
2011-02-09 22:02:07 +0000

    simple unit testing stuff (working on it)
    git-svn-id: http://svn.code.sf.net/p/magicseteditor/code/trunk@1653 0fc631ac-6414-0410-93d0-97cfa31319b6

---
+++ tests/util/MseTestUtils.pm
@@ -0,0 +1,27 @@
+#+----------------------------------------------------------------------------+
+#| Description:  Magic Set Editor - Program to make Magic (tm) cards          |
+#| Copyright:    (C) 2001 - 2011 Twan van Laarhoven and Sean Hunt             |
+#| License:      GNU General Public License 2 or later (see file COPYING)     |
+#+----------------------------------------------------------------------------+
+
+package MseTestUtils;
+
+use strict;
+
+# -----------------------------------------------------------------------------
+# Utilities for testing scripts
+# -----------------------------------------------------------------------------
+
+# Find magicseteditor executable
+
+our $MAGICSETEDITOR;
+my $is_windows = $^O =~ /win/i;
+if ($is_windows) {
+	$MAGICSETEDITOR = "\"../../build/Release Unicode/mse.exe\"";
+} else {
+	$MAGICSETEDITOR = "../../magicseteditor";
+}
+
+
+# -----------------------------------------------------------------------------
+1;
