72b58128226265f667885803bc4c0ca1c6c0e709
twanvl <twanvl@0fc631ac-6414-0410-93d0-97cfa31319b6>
2011-02-09 22:46:00 +0000

    some unit tests that use the scripting language
    git-svn-id: http://svn.code.sf.net/p/magicseteditor/code/trunk@1655 0fc631ac-6414-0410-93d0-97cfa31319b6

--- tests/script/run-tests.pl
+++ tests/script/run-tests.pl
@@ -15,5 +15,5 @@ use MseTestUtils;
 run_script_test("test-builtin.mse-script", "");
 
 write_dummy_set("_dummy-magic-set.mse-set", "game: magic\nstylesheet: new\n");
-run_script_test("test-magic.mse-script", "_dummy-magic-set");
+run_script_test("test-magic.mse-script", "_dummy-magic-set.mse-set");
 remove_dummy_set("_dummy-magic-set.mse-set");
