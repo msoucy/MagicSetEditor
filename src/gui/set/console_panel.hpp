98312816fd3d829dab448b789f50e0c7567fb326
twanvl <twanvl@0fc631ac-6414-0410-93d0-97cfa31319b6>
2012-03-16 23:50:54 +0000

    Console panel improvements:  * added simple history browsing with up/down arrow keys  * show whether MessageCtrl has focus  * don't draw lines between input and resulting errors
    git-svn-id: http://svn.code.sf.net/p/magicseteditor/code/trunk@1738 0fc631ac-6414-0410-93d0-97cfa31319b6

--- src/gui/set/console_panel.hpp
+++ src/gui/set/console_panel.hpp
@@ -14,7 +14,7 @@
 
 class wxSplitterWindow;
 class MessageCtrl;
-class TextCtrl;
+class HistoryTextCtrl;
 
 // ----------------------------------------------------------------------------- : ConsolePanel
 
@@ -46,7 +46,7 @@ class ConsolePanel : public SetWindowPanel {
 	wxSplitterWindow* splitter;
 	MessageCtrl* messages;
 	wxPanel* entry_panel;
-	wxTextCtrl* entry;
+	HistoryTextCtrl* entry;
 	
 	void get_pending_errors();
 	void exec(String const& code);
