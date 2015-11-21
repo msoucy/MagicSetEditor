52b76948089ccd35aeec2d6c5d7a7e12d1a7e650
Matt Soucy <msoucy@csh.rit.edu>
2015-11-21 00:19:10 -0500

    Use builtin/C++11 atomics

--- src/util/smart_ptr.hpp
+++ src/util/smart_ptr.hpp
@@ -15,10 +15,6 @@
 // ----------------------------------------------------------------------------- : Includes
 
 #include <util/atomic.hpp>
-#ifndef HAVE_FAST_ATOMIC
-	/// Using intrusive_ptr where possible? (as opposed to smart_ptr)
-	#error Need fast atomic type for intrusive_ptr
-#endif
 
 // Use slightly less fancy template stuff, so msvc7.1 doesn't crash with an internal compiler error
 #define BOOST_SP_NO_SP_CONVERTIBLE
